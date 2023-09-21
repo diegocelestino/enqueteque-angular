313e160aee706bb0bba8d86a6b1c621dc43eae3d
Diego <diegocdsantos@gmail.com>
2023-09-21 19:06:10 -0300

    preparing for implement the others polls component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { PollComponent } from './main-page/poll/poll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainPageComponent,
+    PollComponent
   ],
   imports: [
     BrowserModule,
